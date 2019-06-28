96ac5bfec3406fccc5962d6b46db399c4853732b
Tanvir Sojal <tanvirahmedsojal@gmail.com>
2019-06-29 01:00:50 +0600

    application 1.0 is complete

---
+++ src/databaseoperations/Exceptions/DataFormatException.java
@@ -0,0 +1,10 @@
+package databaseoperations.Exceptions;
+
+public class DataFormatException extends Exception {
+    public DataFormatException(String message){
+        super(message);
+    }
+    public DataFormatException(){
+        super();
+    }
+}
